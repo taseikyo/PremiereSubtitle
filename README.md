878c94b2771fca0982d58ed47859442e2b163fd3
LewisTian <lewis.smith.tian@gmail.com>
2019-05-17 11:00:30 +0800

    add srt_subtitles.py/exe

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Premiere Subtitle Generator
 <img src="https://i.loli.net/2017/12/15/5a335c9e52d20.png" alt="Psg" width="64">
 
+## Alternative
+由于从 pr cc 2017 就已经不再支持导出旧版字幕了，所以这个工具也宣告失效。当然你若是使用旧版的 pr 且能导出旧版字幕，那么这个小工具依然有效。
+
+另一方面，虽然不支持导出旧版字幕了，但是它支持 srt 字幕，因此我就写了个 Python 脚本，一键生成 srt 字幕。
+
+```bash
+>> git clone https://github.com/LewisTian/PremiereSubtitle.git
+>> cd PremiereSubtitle/dist
+
+>> python srt_subtitles.py 3
+# or 
+>> ./srt_subtitles.exe 3
+```
+
+将你要生成的字幕写入脚本同目录的 txt 文件中，然后双击运行即可。
+
+上面命令中的 `3` 是指定时间间隔，默认是 `5`。
+
 ## Preview
 ![preview.gif](https://i.loli.net/2017/12/03/5a23f6a9390f5.gif)
 
---
+++ dist/srt_subtitles.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-05-02 22:38:28
+# @Author  : Lewis Tian (chtian@hust.edu.cn)
+# @Link    : https://lewistian.github.io/
+# @Version : Python3
+# @Description : generate a srt subtitle for premiere pro cc
+
+from glob import glob
+import sys
+
+def main(interval):
+    for x in glob('*.txt'):
+        h, m, s = 0, 0, 0
+        data = []
+        with open(x, encoding='utf-8') as f:
+            lines = f.readlines()
+        for index, line in enumerate(lines):
+            if s >= 60:
+                s -= 60
+                m += 1
+            if m >= 60:
+                m -= 60
+                h += 1
+            if s + interval >= 60:
+                srt = f'''{index}\n{h}:{m:02}:{s:02}:000 --> {h}:{m+1:02}:{s+interval-60:02}:000\n{line}\n'''
+            else:
+                srt = f'''{index}\n{h}:{m:02}:{s:02}:000 --> {h}:{m:02}:{s+interval:02}:000\n{line}\n'''
+            s += interval
+            print(srt)
+            data.append(srt)
+
+        with open(f"{x}.srt", 'w', encoding='utf-8') as f:
+            f.write(''.join(data))
+
+if __name__ == '__main__':
+    interval = 5
+    if len(sys.argv) > 1:
+        interval = int(sys.argv[1])
+    main(interval)
